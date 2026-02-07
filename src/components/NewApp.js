a22e114e6fad7c8816d13188947e23a00e1c9aa4
Sahu, Gaurav <hashcode.dev@gmail.com>
2026-02-07 18:37:45 +0530

    [XXX-0000]: Creating new Component

---
+++ src/components/NewApp.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function NewApp() {
+    return (
+        <div className="App">
+            <h1>Hello World from new App!</h1>
+        </div>
+    );
+}
+
+export default NewApp;
